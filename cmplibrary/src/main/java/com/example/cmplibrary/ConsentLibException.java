806c2606923b9d4d7c7b10efe1b6392558e4cf0e
André Herculano <andresilveirah@gmail.com>
2018-11-15 16:22:14 +0100

    [MO-146235750] implement ConsentLibException.BuildException

--- cmplibrary/src/main/java/com/example/cmplibrary/ConsentLibException.java
+++ cmplibrary/src/main/java/com/example/cmplibrary/ConsentLibException.java
@@ -1,4 +1,9 @@
 package com.example.cmplibrary;
+
 public class ConsentLibException extends Exception {
     public ConsentLibException(String message) { super(message); }
+
+    public class BuildException extends ConsentLibException {
+        public BuildException(String message) { super("Error during ConsentLib build: "+message); }
+    }
 }
