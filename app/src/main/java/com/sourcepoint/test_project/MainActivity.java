d67ea451e0bd05b4dffce8110d342a2c1b11de28
André Herculano <andresilveirah@gmail.com>
2019-10-08 12:43:26 +0200

    refactor MainActivity

--- app/src/main/java/com/sourcepoint/test_project/MainActivity.java
+++ app/src/main/java/com/sourcepoint/test_project/MainActivity.java
@@ -5,9 +5,8 @@ import android.os.Bundle;
 import android.util.Log;
 
 import com.sourcepoint.cmplibrary.ConsentLib;
-import com.sourcepoint.cmplibrary.CustomVendorConsent;
-import com.sourcepoint.cmplibrary.CustomPurposeConsent;
 import com.sourcepoint.cmplibrary.ConsentLibException;
+import com.sourcepoint.cmplibrary.CustomPurposeConsent;
 
 import java.util.HashSet;
 
@@ -17,52 +16,20 @@ public class MainActivity extends AppCompatActivity {
     private ConsentLib consentLib;
 
     private ConsentLib buildAndRunConsentLib(Boolean showPM) throws ConsentLibException {
-        return ConsentLib.newBuilder(808, "sourcepointnewscript.com", 4601,"5cacf9b2557d160781a25c6a",this)
-       // return ConsentLib.newBuilder(22, "suryakant.com", 2372,"5cacf9b2557d160781a25c6a",this)
+        return ConsentLib.newBuilder(22, "mobile.demo", 2372,"5c0e81b7d74b3c30c6852301",this)
+                .setStage(true)
                 .setViewGroup(findViewById(android.R.id.content))
-                // optional, set custom targeting parameters value can be String and Integer
-                .setTargetingParam("MyPrivacyManager", showPM.toString())
-                //optional,  set message time out , default is 5 seconds
-                .setMessageTimeOut(30000)
+                .setMessageTimeOut(60000)
                 .setShowPM(showPM)
-                .setOnMessageReady(consentLib -> Log.i(TAG, "The message is about to be shown."))
-                .setOnConsentReady(c -> {
-                    Log.d("msgInteraction", "called");
-                    try {
-                        c.getCustomVendorConsents(new String[]{}, result -> {
-                            HashSet<CustomVendorConsent> consents = (HashSet) result;
-                            String myImportantVendorId = "5bf7f5c5461e09743fe190b3";
-                            for (CustomVendorConsent consent : consents)
-                                if (consent.id.equals(myImportantVendorId))
-                                    Log.i(TAG, "Consented to My Important Vendor: " + consent.name);
-                        });
-
-                        c.getCustomPurposeConsents(result -> {
-                            HashSet<CustomPurposeConsent> consents = (HashSet) result;
-                            for (CustomPurposeConsent consent : consents)
-                                Log.i(TAG, "Consented to purpose: " + consent.name);
-                        });
-
-                        // Example usage of getting IAB vendor consent results for a list of vendors
-                        boolean[] IABVendorConsents = c.getIABVendorConsents(new int[]{81, 82});
-                        Log.i(TAG, String.format("Consented to IAB vendors: 81 -> %b, 82 -> %b",
-                                IABVendorConsents[0],
-                                IABVendorConsents[1]
-                        ));
-
-                        // Example usage of getting IAB purpose consent results for a list of purposes
-                        boolean[] IABPurposeConsents = c.getIABPurposeConsents(new int[]{2, 3});
-                        Log.i(TAG, String.format("Consented to IAB purposes: 2 -> %b, 3 -> %b",
-                                IABPurposeConsents[0],
-                                IABPurposeConsents[1]
-                        ));
-
-                    } catch (ConsentLibException e) {
-                        e.printStackTrace();
-                    }
+                .setOnMessageReady(_c -> Log.i(TAG, "onMessageReady"))
+                .setOnConsentReady(consentLib -> {
+                    consentLib.getCustomPurposeConsents(results -> {
+                        HashSet<CustomPurposeConsent> consents = (HashSet) results;
+                        for(CustomPurposeConsent consent : consents)
+                            Log.i(TAG, "Consented to: "+consent);
+                    });
                 })
-                .setOnErrorOccurred(c -> Log.d(TAG, "Something went wrong: ", c.error))
-                // generate ConsentLib at this point modifying builder will not do anything
+                .setOnErrorOccurred(c -> Log.i(TAG, "Something went wrong: ", c.error))
                 .build();
     }
 
