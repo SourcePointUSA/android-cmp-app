d55192e7913607480ddffd157e2e03a5dcaa5acd
Leonardo Doin <leonardo-doin@Leonardos-MacBook-Pro.local>
2020-01-14 18:08:31 +0100

    storeClient refac

--- cmplibrary/src/main/java/com/sourcepoint/cmplibrary/StoreClient.java
+++ cmplibrary/src/main/java/com/sourcepoint/cmplibrary/StoreClient.java
@@ -1,5 +1,7 @@
 package com.sourcepoint.cmplibrary;
 
+import android.content.SharedPreferences;
+
 public class StoreClient {
     /**
      * If the user has consent data stored, reading for this key in the shared preferences will return true
@@ -37,7 +39,41 @@ public class StoreClient {
     @SuppressWarnings("WeakerAccess")
     public static final String CONSENT_UUID_KEY = "consentUUID";
 
+    private SharedPreferences.Editor editor;
 
+    StoreClient(SharedPreferences.Editor editor){
+        this.editor = editor;
+    }
 
+    public void setConsentSubjectToGDPr(Boolean consentSubjectToGDPR){
+        editor.putBoolean(IAB_CONSENT_SUBJECT_TO_GDPR, consentSubjectToGDPR);
+    }
+
+    public void setIabConsentCmpPresent(Boolean iabConsentCmpPresent){
+        editor.putBoolean(IAB_CONSENT_CMP_PRESENT, iabConsentCmpPresent);
+    }
+
+    public void setIabConsentConsentString(String consentConsentString){
+        editor.putString(IAB_CONSENT_CONSENT_STRING, consentConsentString);
+    }
+
+    public void setIabConsentParsedPurposeConsents(String consentParsedPurposeConsents){
+        editor.putString(IAB_CONSENT_PARSED_PURPOSE_CONSENTS, consentParsedPurposeConsents);
+    }
+
+    public void setIabConsentParsedVendorConsents(String consentParsedVendorConsents){
+        editor.putString(IAB_CONSENT_PARSED_VENDOR_CONSENTS, consentParsedVendorConsents);
+    }
+
+    public void setConsentUuid(String consentUuid){
+        editor.putString(CONSENT_UUID_KEY, consentUuid);
+    }
+
+    public void setEuConsent(String euConsent) {
+        editor.putString(EU_CONSENT_KEY, euConsent);
+
+    }
+
+    
 
 }
