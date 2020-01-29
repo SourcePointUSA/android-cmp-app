37cedb1609a0856c82f05636e7c1571449236f0b
Leonardo Doin <leonardo-doin@Leonardos-MacBook-Pro.local>
2020-01-29 15:22:47 +0100

    fix iabConsntString saved with consentUUID value

---
+++ cmplibrary/src/main/java/com/sourcepoint/gdpr_cmplibrary/NativeView.java
@@ -0,0 +1,11 @@
+package com.sourcepoint.gdpr_cmplibrary;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class NativeView extends View {
+    public NativeView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
