cbc9a102886c12bcc917f7c5766670b9b3bcd2b9
Leonardo Doin <leonardo-doin@Leonardos-MacBook-Pro.local>
2020-02-07 13:36:39 -0300

    no setAuthId on axample app

--- app/src/main/java/com/sourcepoint/test_project/MainActivity.java
+++ app/src/main/java/com/sourcepoint/test_project/MainActivity.java
@@ -35,7 +35,6 @@ public class MainActivity extends AppCompatActivity {
     private GDPRConsentLib buildGDPRConsentLib() {
         return GDPRConsentLib.newBuilder(22, "mobile.demo", 2372,"5c0e81b7d74b3c30c6852301",this)
                 .setStagingCampaign(false)
-                .setAuthId("bdddst003")
                 .setTargetingParam("native", "true")
                 .setOnConsentUIReady(view -> {
                     showView(view);
