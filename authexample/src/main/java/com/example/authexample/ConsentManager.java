d386cd12239b930b5f4ebc64358e551141fdbfcb
Leonardo Doin <leonardo-doin@Leonardos-MacBook-Pro.local>
2020-02-19 05:18:27 -0300

    ConsentMenager fixed

--- authexample/src/main/java/com/example/authexample/ConsentManager.java
+++ authexample/src/main/java/com/example/authexample/ConsentManager.java
@@ -1,51 +1,80 @@
 package com.example.authexample;
 
 import android.app.Activity;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.sourcepoint.gdpr_cmplibrary.Consent;
+import com.sourcepoint.gdpr_cmplibrary.ConsentLibBuilder;
+import com.sourcepoint.gdpr_cmplibrary.GDPRConsentLib;
 
 import java.util.HashSet;
 
 abstract class ConsentManager {
     private static final String TAG = "ConsentManager";
 
-    private Activity activity;
+    private ConsentLibBuilder builder;
 
     abstract void onConsentsReady(HashSet<Consent> consents, String consentUUID, String euconsent);
 
     ConsentManager(Activity activity) {
-        this.activity = activity;
+        activity.setContentView(R.layout.activity_home);
+        mainViewGroup = activity.findViewById(android.R.id.content);
+        builder = getBuilder(activity);
     }
 
-//    private ConsentLibBuilder getConsentLib(Boolean pm) {
-//        return ConsentLib.newBuilder(22, "mobile.demo", 2372,"5c0e81b7d74b3c30c6852301",activity)
-//            .setStagingCampaign(true)
-//            .setViewGroup(activity.findViewById(android.R.id.content))
-//            .setShowPM(pm)
-//            .setMessageTimeOut(30000)
-//            .setOnConsentReady(consentLib -> consentLib.getCustomVendorConsents(results -> {
-//                Log.d(TAG, "Interaction complete");
-//                HashSet<Consent> consents = (HashSet) results;
-//                onConsentsReady(consents, consentLib.consentUUID, consentLib.euconsent);
-//            }))
-//            .setConsentUIReady(_c -> Log.d(TAG, "Message Ready"))
-//            .setOnError(c -> Log.d(TAG, "Error Occurred: "+c.error));
-//    }
-//
-//    void loadMessage(Boolean pm) {
-//        try {
-//            getConsentLib(pm).build().run();
-//        } catch (ConsentLibException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//
-//    void loadMessage(boolean pm, String authId) {
-//        try {
-//            getConsentLib(pm).setAuthId(authId).build().run();
-//        } catch (ConsentLibException e) {
-//            e.printStackTrace();
-//        }
-//    }
+    private ViewGroup mainViewGroup;
+
+    private void showView(View view) {
+        if(view.getParent() == null){
+            view.setLayoutParams(new ViewGroup.LayoutParams(0, 0));
+            view.getLayoutParams().height = ViewGroup.LayoutParams.MATCH_PARENT;
+            view.getLayoutParams().width = ViewGroup.LayoutParams.MATCH_PARENT;
+            view.bringToFront();
+            view.requestLayout();
+            mainViewGroup.addView(view);
+        }
+    }
+    private void removeView(View view) {
+        if(view.getParent() != null)
+            mainViewGroup.removeView(view);
+    }
+
+
+    private ConsentLibBuilder getBuilder(Activity a) {
+        return GDPRConsentLib.newBuilder(22, "a-demo-property", 7055,"5c0e81b7d74b3c30c6852301",a)
+                .setStagingCampaign(false)
+                .setOnConsentUIReady(view -> {
+                    showView(view);
+                    Log.i(TAG, "onConsentUIReady");
+                })
+                .setOnConsentUIFinished(view -> {
+                    removeView(view);
+                    Log.i(TAG, "onConsentUIFinished");
+                })
+                .setOnConsentReady(consent -> {
+                    Log.i(TAG, "onConsentReady");
+                    Log.i(TAG, "consentString: " + (consent.consentString != null ? consent.consentString : "<empty>"));
+                    for (String vendorId : consent.acceptedVendors) {
+                        Log.i(TAG, "The vendor " + vendorId + " was accepted.");
+                    }
+                    for (String purposeId : consent.acceptedCategories) {
+                        Log.i(TAG, "The category " + purposeId + " was accepted.");
+                    }
+                })
+                .setOnError(error -> {
+                    Log.e(TAG, "Something went wrong: ", error);
+                    Log.i(TAG, "ConsentLibErrorMessage: " + error.consentLibErrorMessage);
+                });
+    }
+
+    void loadMessage() {
+        builder.build().run();
+    }
+
+    void loadMessage(String authId) {
+        builder.setAuthId(authId).build().run();
+    }
 }
 
