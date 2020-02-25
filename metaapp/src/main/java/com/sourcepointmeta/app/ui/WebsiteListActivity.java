fd284ac3fc2f1959834e001be7e86e07f91a6e1d
CYBAGE\suryakantj <suryakantj@cybage.com>
2020-02-25 14:42:59 +0530

    test commit

--- metaapp/src/main/java/com/sourcepointmeta/app/ui/WebsiteListActivity.java
+++ metaapp/src/main/java/com/sourcepointmeta/app/ui/WebsiteListActivity.java
@@ -37,6 +37,8 @@ import com.sourcepointmeta.app.viewmodel.WebsiteListViewModel;
 
 import java.util.List;
 
+
+// property list activity.
 public class WebsiteListActivity extends BaseActivity<WebsiteListViewModel> {
 
     private final String TAG = "WebsiteListActivity";
