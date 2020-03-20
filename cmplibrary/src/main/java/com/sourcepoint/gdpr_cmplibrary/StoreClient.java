f9241ef25a3b741f305c58504c4b8e0b6fdc9de0
leodoin <leodoin@gmail.com>
2020-03-20 12:30:06 +0100

    wrapping up classes

--- cmplibrary/src/main/java/com/sourcepoint/gdpr_cmplibrary/StoreClient.java
+++ cmplibrary/src/main/java/com/sourcepoint/gdpr_cmplibrary/StoreClient.java
@@ -10,29 +10,9 @@ import java.util.UUID;
 public class StoreClient {
 
     /**
-     * If the user has consent data stored, reading for this key in the shared preferences will return true
-     */
-    public static final String IAB_CONSENT_CMP_PRESENT = "IABConsent_CMPPresent";
-
-    /**
-     * If the user is subject to GDPR, reading for this key in the shared preferences will return "1" otherwise "0"
-     */
-    public static final String IAB_CONSENT_SUBJECT_TO_GDPR = "IABConsent_SubjectToGDPR";
-
-    /**
      * They key used to store the IAB Consent string for the user in the shared preferences
      */
     public static final String IAB_CONSENT_CONSENT_STRING = "IABConsent_ConsentString";
-
-    /**
-     * They key used to read and write the parsed IAB Purposes consented by the user in the shared preferences
-     */
-    public static final String IAB_CONSENT_PARSED_PURPOSE_CONSENTS = "IABConsent_ParsedPurposeConsents";
-
-    /**
-     * They key used to read and write the parsed IAB Vendor consented by the user in the shared preferences
-     */
-    public static final String IAB_CONSENT_PARSED_VENDOR_CONSENTS = "IABConsent_ParsedVendorConsents";
 
 
     public static final String TC_KEYS_KEY = "sp.gdpr.TCKeys";
