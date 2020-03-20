c1b42ff06bf011d0ab143aaddfc4aab794dbff45
leodoin <leodoin@gmail.com>
2020-03-20 14:10:34 +0100

    metaapp tests commented out

--- metaapp/src/androidTest/java/com/sourcepointmeta/app/TestData.java
+++ metaapp/src/androidTest/java/com/sourcepointmeta/app/TestData.java
@@ -7,9 +7,6 @@ import java.util.List;
 
 public class TestData {
 
-    private static final Website WEBSITE_ENTITY = new Website(22,"mobile.demo",false);
-    private static final Website WEBSITE_ENTITY2 = new Website(808,"AndroidTesting",false);
-
-    public static final List<Website> WEBSITES = Arrays.asList(WEBSITE_ENTITY, WEBSITE_ENTITY2);
+    public static final List<Website> WEBSITES = Arrays.asList();
 
 }
