5e6414c01e3574661913ff803e61e944e7341ecd
leodoin <leodoin@gmail.com>
2020-04-14 11:55:37 +0200

    Create ActionTypes.java

---
+++ cmplibrary/src/main/java/com/sourcepoint/gdpr_cmplibrary/ActionTypes.java
@@ -0,0 +1,11 @@
+package com.sourcepoint.gdpr_cmplibrary;
+
+public class ActionTypes {
+
+    public static final int SHOW_OPTIONS = 12;
+    public static final int REJECT_ALL = 13;
+    public static final int ACCEPT_ALL = 11;
+    public static final int MSG_CANCEL = 15;
+    public static final int SAVE_AND_EXIT = 1;
+    public static final int PM_DISMISS = 2;
+}
