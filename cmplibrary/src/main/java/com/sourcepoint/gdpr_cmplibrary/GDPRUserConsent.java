e56e83f47f04e1f54b0a84a01b061c8a399dcb14
CYBAGE\suryakantj <suryakantj@cybage.com>
2020-05-25 13:27:05 +0530

    fixed exmple crash due to empty userconsent object

--- cmplibrary/src/main/java/com/sourcepoint/gdpr_cmplibrary/GDPRUserConsent.java
+++ cmplibrary/src/main/java/com/sourcepoint/gdpr_cmplibrary/GDPRUserConsent.java
@@ -13,13 +13,13 @@ public class GDPRUserConsent {
     public JSONObject jsonConsents;
     public String consentString;
 
-    public GDPRUserConsent(){
-        acceptedVendors = new ArrayList<>();
-        acceptedCategories = new ArrayList<>();
-        consentString = "";
+    private static JSONObject emptyConsents = null;
+
+    public GDPRUserConsent() throws JSONException {
+        this(getEmptyUserConsents());
     }
 
-    public GDPRUserConsent(JSONObject jConsent) throws JSONException, ConsentLibException {
+    public GDPRUserConsent(JSONObject jConsent) throws JSONException {
         this.acceptedVendors = json2StrArr(jConsent.getJSONArray("acceptedVendors"));
         this.acceptedCategories = json2StrArr(jConsent.getJSONArray("acceptedCategories"));
         if(jConsent.has("euconsent") && !jConsent.isNull("euconsent")){
@@ -40,5 +40,16 @@ public class GDPRUserConsent {
 
     public JSONObject getJsonConsents() {
         return jsonConsents;
+    }
+
+    private static JSONObject getEmptyUserConsents() throws JSONException{
+        if (emptyConsents  !=null)
+            return emptyConsents;
+        emptyConsents = new JSONObject();
+        emptyConsents.put("acceptedVendors",new JSONArray());
+        emptyConsents.put("acceptedCategories",new JSONArray());
+        emptyConsents.put("euconsent","");
+
+        return emptyConsents;
     }
 }
