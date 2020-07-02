189869b39e0c0aa6b1418d24e2a744bb116964d6
Vrushali Deshpande <vrushalid@cybage.com>
2020-07-02 14:39:29 +0530

    [SP-4077] Removed Thread.sleep and used explicitly wait method and implement log messages

--- AutomationFramework/src/main/java/org/framework/pageObjects/NewSitePage.java
+++ AutomationFramework/src/main/java/org/framework/pageObjects/NewSitePage.java
@@ -14,7 +14,6 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import io.appium.java_client.HidesKeyboard;
-//import io.appium.java_client.WebElement;
 import io.appium.java_client.PerformsTouchActions;
 import io.appium.java_client.TouchAction;
 import io.appium.java_client.pagefactory.AndroidFindBy;
@@ -84,133 +83,18 @@ public class NewSitePage extends Page {
 	@AndroidFindBy(id = "com.sourcepointmeta.app:id/toggleShowPM")
 	public WebElement GDPRShowPrivacyManager;
 
-//	public NewSitePage(WebDriver driver) throws InterruptedException {
-//        this.driver = driver;
-//        PageFactory.initElements(driver, this);
-//        logMessage("Initializing the "+this.getClass().getSimpleName()+" elements");
-//        PageFactory.initElements(new AppiumFieldDecorator(driver), this);
-//        Thread.sleep(1000);
-//    }
-
 	boolean paramFound = false;
 
-//	public void addNewSiteWithOutParameter(String accountId, String siteName, String staggingValue)
-//			throws InterruptedException {
-//		AccountID.sendKeys(accountId);
-//		SiteName.sendKeys(siteName);
-//
-//		if (staggingValue.equals("ON")) {
-//			Point point = ToggleButton.getLocation();
-//			TouchAction touchAction = new TouchAction((PerformsTouchActions) driver);
-//
-//			touchAction.tap(PointOption.point(point.x + 20, point.y + 20)).perform();
-//		}
-//
-//		SaveButton.click();
-//		Thread.sleep(3000);
-//	}
-//
-//	public void addNewSite(String accountId, String siteName, String staggingValue) throws InterruptedException {
-//		AccountID.clear();
-//		AccountID.sendKeys(accountId);
-//		//SiteName.clear();
-//		SiteName.sendKeys(siteName);
-//
-//		if (staggingValue.equals("ON")) {
-//			System.out.println("########inside########");
-//			Point point = ToggleButton.getLocation();
-//			
-//			TouchAction touchAction = new TouchAction((PerformsTouchActions) driver);
-//
-//			touchAction.tap(PointOption.point(point.x + 20, point.y + 20)).perform();
-//		}
-//	}
-//
-//	public void addTargetingParameter(String key, String value) {
-//		ParameterKey.clear();
-//		ParameterKey.sendKeys(key);
-//		ParameterValue.clear();
-//		ParameterValue.sendKeys(value);
-//		ParameterAddButton.click();
-//
-//	}
-//
-//	public void removeTargetingParameter() {
-//
-//	}
-//
-//	public boolean isParameterAdded(String key, String value) {
-//		try {
-//			String data = TargetingParam.getText();
-//			if (data.split(" : ")[0].equals(key) && data.split(" : ")[1].equals(value))
-//				paramFound = true;
-//			System.out.println(key + " : " + value + " is present.");
-//		} catch (Exception e) {
-//			paramFound = false;
-//		}
-//		return paramFound;
-//	}
-//
-//	public void isParameterRemoved() {
-//
-//	}
-//
-//	public void isParameterUpdated() {
-//
-//	}
-//
-//	public boolean verifyError(String pName) throws InterruptedException {
-//		boolean check = false;
-//		// waitForElement(ErrorMessage, 10);
-//		Thread.sleep(3000);
-//		int i = ErrorMessage.size();
-//		
-//		String errorMsg = ErrorMessage.get(ErrorMessage.size() - 1).getText();
-//		
-//		if (pName.equalsIgnoreCase(PlatformName.ANDROID.toString())) {
-//			if (errorMsg.equals("Please enter targeting param Key/Value"))
-//				check = true;				
-//		}else {
-//			if (errorMsg.equals("Please enter targeting parameter key and value"))
-//				check = true;				
-//		}
-//		return check;
-//	}
-//
-//	public String verifyErrorMsg() throws InterruptedException {
-//		Thread.sleep(3000);
-//		int i = ErrorMessage.size();
-//		
-//		return ErrorMessage.get(ErrorMessage.size() - 1).getText();
-//	}
-//	
-//	public void waitForElement(WebElement ele, int timeOutInSeconds) {
-//		WebDriverWait wait = new WebDriverWait(driver, timeOutInSeconds);
-//		wait.until(ExpectedConditions.visibilityOf(ele));
-//	}
-//	
-//	public void clearAccountId() {
-//		AccountID.clear();
-//	}
-//
-//	public void clearSiteName() {
-//		SiteName.clear();
-//	}
-
 	public void selectCampaign(WebElement ele, String staggingValue) throws InterruptedException {
-
 		if (staggingValue.equals("ON")) {
 			Point point = ele.getLocation();
 			TouchAction touchAction = new TouchAction((PerformsTouchActions) driver);
-
 			touchAction.tap(PointOption.point(point.x + 20, point.y + 20)).perform();
 		}
-		Thread.sleep(3000);
 	}
 
 	public void addTargetingParameter(WebElement paramKey, WebElement paramValue, String key, String value)
 			throws InterruptedException {
-
 		((HidesKeyboard) driver).hideKeyboard();
 		paramKey.sendKeys(key);
 
@@ -220,50 +104,9 @@ public class NewSitePage extends Page {
 		((HidesKeyboard) driver).hideKeyboard();
 	}
 
-	public String getError() throws InterruptedException {
-		boolean check = false;
-		// waitForElement(ErrorMessage, 10);
-		Thread.sleep(5000);
-		int i = ErrorMessage.size();
-
-		String errorMsg = ErrorMessage.get(ErrorMessage.size() - 1).getText();
-		return errorMsg;
-	}
-
-	public boolean verifyError() throws InterruptedException {
-		boolean check = false;
-		// waitForElement(ErrorMessage, 10);
-		Thread.sleep(3000);
-		int i = ErrorMessage.size();
-
-		String errorMsg = ErrorMessage.get(ErrorMessage.size() - 1).getText();
-		if (errorMsg.equals("Please enter targeting parameter key and value")) {
-			check = true;
-		}
-		return check;
-	}
-
-//	public boolean verifyError() {
-//		return paramFound;
-//
-//	}
-
 	public void waitForElement(WebElement ele, int timeOutInSeconds) {
 		WebDriverWait wait = new WebDriverWait(driver, timeOutInSeconds);
 		wait.until(ExpectedConditions.visibilityOf(ele));
 	}
 
-	public boolean verifyErrorMsg(String udid) throws InterruptedException {
-		boolean check = false;
-		// waitForElement(ErrorMessage, 10);
-		Thread.sleep(3000);
-		int i = ErrorMessage.size();
-
-		String errorMsg = ErrorMessage.get(ErrorMessage.size() - 1).getText();
-
-		if (errorMsg.equals("Please enter property details")) {
-			check = true;
-		}
-		return check;
-	}
 }
