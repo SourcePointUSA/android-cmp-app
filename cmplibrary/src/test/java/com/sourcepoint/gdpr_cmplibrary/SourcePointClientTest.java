0455152d9fd4d25fd5a2d7ee223764a4220a46e5
CYBAGE\suryakantj <suryakantj@cybage.com>
2020-07-16 16:04:07 +0530

    okhttp client mock implementation using mockito framework..

--- cmplibrary/src/test/java/com/sourcepoint/gdpr_cmplibrary/SourcePointClientTest.java
+++ cmplibrary/src/test/java/com/sourcepoint/gdpr_cmplibrary/SourcePointClientTest.java
@@ -11,23 +11,31 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
 
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
-import okhttp3.mockwebserver.MockResponse;
-import okhttp3.mockwebserver.MockWebServer;
+import okhttp3.Protocol;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.reset;
-import static org.mockito.Mockito.timeout;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -39,29 +47,30 @@ public class SourcePointClientTest {
     private SourcePointClient sourcePointClient;
     private GDPRConsentLib.OnLoadComplete onLoadComplete;
 
-    private MockWebServer webServerMock;
-
     private static final String consentUUID = "consentUUID";
     private static final String euConsent = "euConsent";
     private static final String meta = "meta";
     private static final boolean isNative = false;
     private static NetworkInfo networkInfo;
     private static ConnectivityManager connectivityManager;
-    private static long timeout = 1000;
 
+    OkHttpClient http;
+    Call remoteCall;
 
     @Before
-    public void setUp() throws IOException {
-        OkHttpClient http = new OkHttpClient();
-
+    public void setUp() {
+        remoteCall = mock(Call.class);
+        http = getOkHttpClientMock();
         onLoadComplete = mock(GDPRConsentLib.OnLoadComplete.class);
-
         networkInfo = getNetworkInfo();
         connectivityManager = getConnectivityManager();
-
         sourcePointClient =  new SourcePointClient(http, getSourcePointClientConfig(), connectivityManager);
-        webServerMock = new MockWebServer();
-        webServerMock.start();
+    }
+
+    private OkHttpClient getOkHttpClientMock(){
+        OkHttpClient okhttp = mock(OkHttpClient.class);
+        when(okhttp.newCall(any())).thenReturn(remoteCall);
+        return okhttp;
     }
 
     private SourcePointClientConfig getSourcePointClientConfig(){
@@ -86,19 +95,39 @@ public class SourcePointClientTest {
     }
 
     @After
-    public void finish() throws IOException {
-        webServerMock.shutdown();
+    public void finish() {
         reset(onLoadComplete);
         reset(networkInfo);
         reset(connectivityManager);
+        reset(http);
+        reset(remoteCall);
+    }
+
+    private void doAPICallWithAnswer(final boolean isSuccess , final int responseCode, final String responseString){
+        doAnswer(new Answer() {
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                Callback callback = invocation.getArgument(0);
+                final Response response = new Response.Builder()
+                        .request(new Request.Builder().url("http://url.com").build())
+                        .protocol(Protocol.HTTP_1_1)
+                        .code(responseCode).message("").body(
+                                ResponseBody.create(
+                                        MediaType.parse(""),
+                                        responseString
+                                ))
+                        .build();
+                if (isSuccess)
+                    callback.onResponse(remoteCall , response);
+                else callback.onFailure(remoteCall , new IOException(new ConsentLibException("A failure while API call ")));
+                return null;
+            }
+        }).when(remoteCall).enqueue(any(Callback.class));
     }
 
     @Test
     public void noInternetErrorWhileGetMessage() {
         when(networkInfo.isConnectedOrConnecting()).thenReturn(false);
-        webServerMock.enqueue(new MockResponse().setResponseCode(HttpURLConnection.HTTP_OK));
-        sourcePointClient.baseMsgUrl = webServerMock.url("/").toString();
-
         try {
             sourcePointClient.getMessage(isNative, consentUUID, meta, euConsent, onLoadComplete);
         } catch (ConsentLibException e) {
@@ -109,12 +138,12 @@ public class SourcePointClientTest {
     @Test
     public void getMessageSuccess() throws ConsentLibException {
         String responseString = "https://notice.sp-prod.net?message_id=162961";
-        webServerMock.enqueue(new MockResponse().setBody(responseString).setResponseCode(HttpURLConnection.HTTP_OK));
-        sourcePointClient.baseMsgUrl = webServerMock.url("/").toString();
+
+        doAPICallWithAnswer(true , HttpURLConnection.HTTP_OK, responseString);
 
         sourcePointClient.getMessage(isNative, consentUUID, meta, euConsent, onLoadComplete);
         ArgumentCaptor<Object> captor = ArgumentCaptor.forClass(Object.class);
-        verify(onLoadComplete, timeout(timeout)).onSuccess(captor.capture());
+        verify(onLoadComplete).onSuccess(captor.capture());
         String response = captor.getValue().toString();
         assertEquals(responseString, response);
     }
@@ -122,20 +151,30 @@ public class SourcePointClientTest {
     @Test
     public void getMessageFailure() throws ConsentLibException {
         String errorMessage = "Fail to get message from: ";
-        webServerMock.enqueue(new MockResponse().setResponseCode(HttpURLConnection.HTTP_BAD_REQUEST));
-        sourcePointClient.baseMsgUrl = webServerMock.url("/").toString();
+
+        doAPICallWithAnswer(true , HttpURLConnection.HTTP_BAD_REQUEST, errorMessage);
 
         sourcePointClient.getMessage(isNative ,consentUUID, meta , euConsent, onLoadComplete);
         ArgumentCaptor<ConsentLibException> captor = ArgumentCaptor.forClass(ConsentLibException.class);
-        verify(onLoadComplete, timeout(timeout)).onFailure(captor.capture());
+        verify(onLoadComplete).onFailure(captor.capture());
         assertTrue(captor.getValue().consentLibErrorMessage.contains(errorMessage));
 }
 
     @Test
+    public void getMessageOnFailureCallBack() throws ConsentLibException {
+        String errorMessage = "Fail to get message from: ";
+
+        doAPICallWithAnswer(false , HttpURLConnection.HTTP_BAD_REQUEST, errorMessage);
+
+        sourcePointClient.getMessage(isNative ,consentUUID, meta , euConsent, onLoadComplete);
+        ArgumentCaptor<ConsentLibException> captor = ArgumentCaptor.forClass(ConsentLibException.class);
+        verify(onLoadComplete).onFailure(captor.capture());
+        assertTrue(captor.getValue().consentLibErrorMessage.contains(errorMessage));
+    }
+
+    @Test
     public void noInternetErrorWhileSendConsent() {
         when(networkInfo.isConnectedOrConnecting()).thenReturn(false);
-        webServerMock.enqueue(new MockResponse().setResponseCode(HttpURLConnection.HTTP_OK));
-        sourcePointClient.baseSendConsentUrl = webServerMock.url("/").toString();
 
         try {
             sourcePointClient.sendConsent(mock(JSONObject.class), onLoadComplete);
@@ -147,8 +186,6 @@ public class SourcePointClientTest {
     @Test
     public void errorWhileSendConsent() throws JSONException {
         String errorMessage = "Error adding param requestUUID.";
-        webServerMock.enqueue(new MockResponse().setResponseCode(HttpURLConnection.HTTP_OK));
-        sourcePointClient.baseSendConsentUrl = webServerMock.url("/").toString();
         JSONObject jsonMock = mock(JSONObject.class);
         doThrow(JSONException.class).when(jsonMock).put(anyString(),anyString());
         try {
@@ -161,12 +198,11 @@ public class SourcePointClientTest {
     @Test
     public void sendConsentSuccess() throws ConsentLibException {
         String responseString = "A dummy response string";
-        webServerMock.enqueue(new MockResponse().setBody(responseString).setResponseCode(HttpURLConnection.HTTP_OK));
-        sourcePointClient.baseSendConsentUrl = webServerMock.url("/").toString();
+        doAPICallWithAnswer(true, HttpURLConnection.HTTP_OK, responseString);
 
         sourcePointClient.sendConsent(mock(JSONObject.class), onLoadComplete);
         ArgumentCaptor<Object> captor = ArgumentCaptor.forClass(Object.class);
-        verify(onLoadComplete, timeout(timeout)).onSuccess(captor.capture());
+        verify(onLoadComplete).onSuccess(captor.capture());
         String response = captor.getValue().toString();
         assertEquals(responseString,response);
     }
@@ -174,21 +210,28 @@ public class SourcePointClientTest {
     @Test
     public void sendConsentFailure() throws ConsentLibException {
         String errorMessage = "Fail to send consent to: ";
-        webServerMock.enqueue(new MockResponse().setResponseCode(HttpURLConnection.HTTP_BAD_REQUEST));
-        sourcePointClient.baseSendConsentUrl = webServerMock.url("/").toString();
+        doAPICallWithAnswer(true, HttpURLConnection.HTTP_BAD_REQUEST, errorMessage);
 
         sourcePointClient.sendConsent(mock(JSONObject.class), onLoadComplete);
         ArgumentCaptor<ConsentLibException> captor = ArgumentCaptor.forClass(ConsentLibException.class);
-        verify(onLoadComplete, timeout(timeout)).onFailure(captor.capture());
+        verify(onLoadComplete).onFailure(captor.capture());
+        assertTrue(captor.getValue().consentLibErrorMessage.contains(errorMessage));
+    }
+
+    @Test
+    public void sendConsentOnFailureCallback() throws ConsentLibException {
+        String errorMessage = "Fail to send consent to: ";
+        doAPICallWithAnswer(false, HttpURLConnection.HTTP_BAD_REQUEST, errorMessage);
+
+        sourcePointClient.sendConsent(mock(JSONObject.class), onLoadComplete);
+        ArgumentCaptor<ConsentLibException> captor = ArgumentCaptor.forClass(ConsentLibException.class);
+        verify(onLoadComplete).onFailure(captor.capture());
         assertTrue(captor.getValue().consentLibErrorMessage.contains(errorMessage));
     }
 
     @Test
     public void noInternetErrorWhileSendCustomConsents() {
         when(networkInfo.isConnectedOrConnecting()).thenReturn(false);
-        webServerMock.enqueue(new MockResponse().setResponseCode(HttpURLConnection.HTTP_OK));
-        sourcePointClient.baseSendCustomConsentsUrl = webServerMock.url("/").toString();
-
         try {
             sourcePointClient.sendCustomConsents(mock(JSONObject.class), onLoadComplete);
         } catch (ConsentLibException e) {
@@ -200,8 +243,6 @@ public class SourcePointClientTest {
     @Test
     public void errorWhileSendCustomConsent() throws JSONException {
         String errorMessage = "Error adding param requestUUID.";
-        webServerMock.enqueue(new MockResponse().setResponseCode(HttpURLConnection.HTTP_OK));
-        sourcePointClient.baseSendCustomConsentsUrl = webServerMock.url("/").toString();
         JSONObject jsonMock = mock(JSONObject.class);
         doThrow(JSONException.class).when(jsonMock).put(anyString(),anyString());
         try {
@@ -214,25 +255,34 @@ public class SourcePointClientTest {
     @Test
     public void sendCustomConsentsSuccess() throws ConsentLibException {
         String responseString = "A dummy response string";
-        webServerMock.enqueue(new MockResponse().setBody(responseString).setResponseCode(HttpURLConnection.HTTP_OK));
-        sourcePointClient.baseSendCustomConsentsUrl = webServerMock.url("/").toString();
+        doAPICallWithAnswer(true, HttpURLConnection.HTTP_OK, responseString);
 
         sourcePointClient.sendCustomConsents(mock(JSONObject.class), onLoadComplete);
         ArgumentCaptor<Object> captor = ArgumentCaptor.forClass(Object.class);
-        verify(onLoadComplete, timeout(timeout)).onSuccess(captor.capture());
+        verify(onLoadComplete).onSuccess(captor.capture());
         String response = captor.getValue().toString();
         assertEquals(responseString, response);
     }
 
     @Test
     public void sendCustomConsentsFailure() throws ConsentLibException {
-        String errorMessage ="Fail to send consent to: " ;
-        webServerMock.enqueue(new MockResponse().setResponseCode(HttpURLConnection.HTTP_BAD_REQUEST));
-        sourcePointClient.baseSendCustomConsentsUrl = webServerMock.url("/").toString();
+        String errorMessage ="Fail to send consent to: ";
+        doAPICallWithAnswer(true, HttpURLConnection.HTTP_BAD_REQUEST, errorMessage);
 
         sourcePointClient.sendCustomConsents(mock(JSONObject.class), onLoadComplete);
         ArgumentCaptor<ConsentLibException> captor = ArgumentCaptor.forClass(ConsentLibException.class);
-        verify(onLoadComplete, timeout(timeout)).onFailure(captor.capture());
+        verify(onLoadComplete).onFailure(captor.capture());
+        assertTrue(captor.getValue().consentLibErrorMessage.contains(errorMessage));
+    }
+
+    @Test
+    public void sendCustomConsentsOnFailureCallback() throws ConsentLibException {
+        String errorMessage ="Fail to send consent to: " ;
+        doAPICallWithAnswer(false, HttpURLConnection.HTTP_BAD_REQUEST, errorMessage);
+
+        sourcePointClient.sendCustomConsents(mock(JSONObject.class), onLoadComplete);
+        ArgumentCaptor<ConsentLibException> captor = ArgumentCaptor.forClass(ConsentLibException.class);
+        verify(onLoadComplete).onFailure(captor.capture());
         assertTrue(captor.getValue().consentLibErrorMessage.contains(errorMessage));
     }
 }
