4c0392993fb9463122aff12db3aac7190264fe63
leodoin <leodoin@gmail.com>
2020-07-29 17:09:28 +0200

    Update ConsentAction.java
    add constructor w/ pmId

--- cmplibrary/src/main/java/com/sourcepoint/gdpr_cmplibrary/ConsentAction.java
+++ cmplibrary/src/main/java/com/sourcepoint/gdpr_cmplibrary/ConsentAction.java
@@ -7,19 +7,25 @@ public class ConsentAction {
 
     public final ActionTypes actionType;
     public final String choiceId;
+    public final String privacyManagerId;
     public final boolean requestFromPm;
     public final JSONObject pmSaveAndExitVariables;
 
-    ConsentAction(int actionType, String choiceId, boolean requestFromPm, JSONObject pmSaveAndExitVariables) {
+    ConsentAction(int actionType, String choiceId, String privacyManagerId, boolean requestFromPm, JSONObject pmSaveAndExitVariables) {
         this.actionType = ActionTypes.valueOf(actionType);
         this.choiceId = choiceId;
+        this.privacyManagerId = privacyManagerId;
         this.requestFromPm = requestFromPm;
         this.pmSaveAndExitVariables = pmSaveAndExitVariables;
     }
 
-    public static ConsentAction getEmptyDismissAction(boolean isPmOn){
-        return isPmOn ? new ConsentAction(ActionTypes.PM_DISMISS.code, "", true, new JSONObject())
-                : new ConsentAction(ActionTypes.MSG_CANCEL.code, "", false, new JSONObject());
+    ConsentAction(int actionType, String choiceId, boolean requestFromPm, JSONObject pmSaveAndExitVariables) {
+        this(actionType, choiceId, null, requestFromPm, pmSaveAndExitVariables);
+    }
+
+    public static ConsentAction getEmptyDismissAction(boolean isPmOn) {
+        return isPmOn ? new ConsentAction(ActionTypes.PM_DISMISS.code, "", "", true, new JSONObject())
+                : new ConsentAction(ActionTypes.MSG_CANCEL.code, "", "", false, new JSONObject());
     }
 
 }
