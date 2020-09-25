74b92ab7bf11c2cba214bc3b4e8ed6cfb693a3de
CYBAGE\suryakantj <suryakantj@cybage.com>
2020-09-25 14:51:02 +0530

    MetaApp unit test update

--- metaapp/src/test/java/com/sourcepointmeta/metaapp/repository/PropertyListRepositoryTest.java
+++ metaapp/src/test/java/com/sourcepointmeta/metaapp/repository/PropertyListRepositoryTest.java
@@ -88,11 +88,11 @@ public class PropertyListRepositoryTest {
         Property property = StaticTestData.PROPERTIES.get(0);
         property.setId(1);
 
-        propertyListDao.update(property.getAccountID(), property.getPropertyID() , property.getProperty(), property.getPmID() , property.isStaging(), property.isShowPM(), property.getAuthId(), property.getId());
-        verify(propertyListDao).update(property.getAccountID(), property.getPropertyID(), property.getProperty(), property.getPmID(), property.isStaging(), property.isShowPM(), property.getAuthId(), property.getId());
+        propertyListDao.update(property.getAccountID(), property.getPropertyID() , property.getProperty(), property.getPmID() , property.isStaging(), property.isNative(), property.getAuthId(), property.getId());
+        verify(propertyListDao).update(property.getAccountID(), property.getPropertyID(), property.getProperty(), property.getPmID(), property.isStaging(), property.isNative(), property.getAuthId(), property.getId());
 
-        when(propertyListDao.update(property.getAccountID(), property.getPropertyID() , property.getProperty(), property.getPmID(), property.isStaging(), property.isShowPM(), property.getAuthId(), property.getId())).thenReturn(1);
-        assertEquals(1, propertyListDao.update(property.getAccountID(), property.getPropertyID(), property.getProperty(), property.getPmID(), property.isStaging(), property.isShowPM(), property.getAuthId(), property.getId()));
+        when(propertyListDao.update(property.getAccountID(), property.getPropertyID() , property.getProperty(), property.getPmID(), property.isStaging(), property.isNative(), property.getAuthId(), property.getId())).thenReturn(1);
+        assertEquals(1, propertyListDao.update(property.getAccountID(), property.getPropertyID(), property.getProperty(), property.getPmID(), property.isStaging(), property.isNative(), property.getAuthId(), property.getId()));
     }
 
     @Test
@@ -129,13 +129,13 @@ public class PropertyListRepositoryTest {
         }
 
         if (keyList.isEmpty()){
-            when(propertyListDao.getPropertyWithDetails(property.getAccountID(), property.getPropertyID(), property.getProperty(), property.getPmID() , property.isStaging(), property.isShowPM() , property.getAuthId())).thenReturn(1);
+            when(propertyListDao.getPropertyWithDetails(property.getAccountID(), property.getPropertyID(), property.getProperty(), property.getPmID() , property.isStaging(), property.isNative() , property.getAuthId())).thenReturn(1);
         }else {
             TargetingParameterList targetingParameterList = new TargetingParameterList();
             targetingParameterList.setKeyList(keyList);
             targetingParameterList.setValueList(valueList);
             List<TargetingParameterList> targetingParameterLists = Arrays.asList(targetingParameterList);
-            when(propertyListDao.getPropertyWithDetails(property.getAccountID(), property.getPropertyID(), property.getProperty(), property.getPmID(), property.isStaging(), property.isShowPM() , property.getAuthId(),
+            when(propertyListDao.getPropertyWithDetails(property.getAccountID(), property.getPropertyID(), property.getProperty(), property.getPmID(), property.isStaging(), property.isNative() , property.getAuthId(),
                     keyList, valueList)).thenReturn(targetingParameterLists);
         }
 
@@ -155,7 +155,7 @@ public class PropertyListRepositoryTest {
 
         Property property = StaticTestData.PROPERTIES.get(2);
         Observer<Integer> observer = mock(Observer.class);
-        doReturn(1).when(propertyListDao).getPropertyWithDetails(property.getAccountID(), property.getPropertyID(), property.getProperty(), property.getPmID() , property.isStaging(), property.isShowPM(), property.getAuthId());
+        doReturn(1).when(propertyListDao).getPropertyWithDetails(property.getAccountID(), property.getPropertyID(), property.getProperty(), property.getPmID() , property.isStaging(), property.isNative(), property.getAuthId());
 
 
         MutableLiveData<Integer> listSize = (MutableLiveData<Integer>) propertyListRepository.getPropertyWithDetails(property);
