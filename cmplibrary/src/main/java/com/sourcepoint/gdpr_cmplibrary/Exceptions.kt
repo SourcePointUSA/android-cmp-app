34d9c1fbfaa47fb8fd2f593389d84fa5148a4699
Sourcepoint <sourcepoint@carmelos-mbp.localdomain>
2020-12-09 15:14:53 +0100

    exception created

--- cmplibrary/src/main/java/com/sourcepoint/gdpr_cmplibrary/Exceptions.kt
+++ cmplibrary/src/main/java/com/sourcepoint/gdpr_cmplibrary/Exceptions.kt
@@ -2,34 +2,82 @@ package com.sourcepoint.gdpr_cmplibrary
 
 import java.lang.RuntimeException
 
-internal sealed class ConsentLibExceptionK : RuntimeException{
-    constructor(code : String, description : String) : super()
-    constructor(code : String, description : String, cause: Throwable?) : super(cause)
-}
+internal sealed class ConsentLibExceptionK(
+    val details: String,
+    val code: String,
+    val description: String,
+    cause: Throwable? = null
+) : RuntimeException(cause)
 
-internal class NoInternetConnectionException : ConsentLibExceptionK {
-    companion object{
-        const val code = ""
-        const val description : String = ""
-    }
-    constructor() : super(code, description)
-    constructor(cause: Throwable?) : super(code, description, cause)
-}
+internal class NoInternetConnectionException(
+    details: String,
+    cause: Throwable? = null
+) : ConsentLibExceptionK(
+    details = details,
+    cause = cause,
+    code = "",
+    description = "")
 
-internal class ApiException : ConsentLibExceptionK {
-    companion object{
-        const val code = ""
-        const val description : String = ""
-    }
-    constructor() : super(code, description)
-    constructor(cause: Throwable?) : super(code, description, cause)
-}
+internal class ServerInternalException(
+    details: String,
+    cause: Throwable? = null
+) : ConsentLibExceptionK(
+    details = details,
+    cause = cause,
+    code = "",
+    description = "")
 
-internal class ParseException : ConsentLibExceptionK {
-    companion object{
-        const val code = ""
-        const val description : String = ""
-    }
-    constructor() : super(code, description)
-    constructor(cause: Throwable?) : super(code, description, cause)
-}
+internal class WebViewOnErrorException(
+    details: String,
+    cause: Throwable? = null
+) : ConsentLibExceptionK(
+    details = details,
+    cause = cause,
+    code = "",
+    description = "")
+
+internal class UrlLoadingException(
+    details: String,
+    cause: Throwable? = null
+) : ConsentLibExceptionK(
+    details = details,
+    cause = cause,
+    code = "",
+    description = "")
+
+internal class EventPayloadException(
+    details: String,
+    cause: Throwable? = null
+) : ConsentLibExceptionK(
+    details = details,
+    cause = cause,
+    code = "",
+    description = "")
+
+internal class RemoteConfigException(
+    details: String,
+    cause: Throwable? = null
+) : ConsentLibExceptionK(
+    details = details,
+    cause = cause,
+    code = "",
+    description = "")
+
+internal class ResourceNotFoundException(
+    details: String,
+    cause: Throwable? = null
+) : ConsentLibExceptionK(
+    details = details,
+    cause = cause,
+    code = "",
+    description = "")
+
+internal class ConnectionTimeoutException(
+    details: String,
+    cause: Throwable? = null
+) : ConsentLibExceptionK(
+    details = details,
+    cause = cause,
+    code = "",
+    description = "")
+
