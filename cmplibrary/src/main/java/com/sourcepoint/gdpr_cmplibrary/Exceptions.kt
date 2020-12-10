addb22a72df6017100c5ceef1cffac83bacc794e
Sourcepoint <sourcepoint@carmelos-mbp.localdomain>
2020-12-10 11:37:31 +0100

    added tests for message

--- cmplibrary/src/main/java/com/sourcepoint/gdpr_cmplibrary/Exceptions.kt
+++ cmplibrary/src/main/java/com/sourcepoint/gdpr_cmplibrary/Exceptions.kt
@@ -3,88 +3,99 @@ package com.sourcepoint.gdpr_cmplibrary
 internal sealed class ConsentLibExceptionK(
     val description: String,
     cause: Throwable? = null
-) : RuntimeException(cause){
+) : RuntimeException(cause) {
     abstract val code: String
+
+    override fun toString(): String {
+        return """
+            {
+                "code" : "$code",
+                "message" : "${cause?.message ?: ""}",
+                "description" : "$description",
+                "class_type" : "${this::class.java}"
+            }
+        """.trimIndent()
+    }
 }
 
-internal class NoInternetConnectionException(
+internal class NoInternetConnectionException @JvmOverloads constructor(
     cause: Throwable? = null,
     description: String
 ) : ConsentLibExceptionK(
     cause = cause,
-    description = description){
+    description = description) {
     override val code: String = "no_internet_connection"
 }
 
-internal class InternalServerException(
+internal class InternalServerException @JvmOverloads constructor(
     cause: Throwable? = null,
     description: String
 ) : ConsentLibExceptionK(
     cause = cause,
-    description = description){
+    description = description) {
     override val code: String = "internal_server_error"
 }
 
-internal class WebViewException(
+internal class WebViewException @JvmOverloads constructor(
     cause: Throwable? = null,
     description: String
 ) : ConsentLibExceptionK(
     cause = cause,
-    description = description){
+    description = description) {
     override val code: String = "web_view_error"
 }
 
-internal class UrlLoadingException(
+internal class UrlLoadingException @JvmOverloads constructor(
     cause: Throwable? = null,
     description: String
 ) : ConsentLibExceptionK(
     cause = cause,
-    description = description){
+    description = description) {
     override val code: String = "url_loading_error"
 }
 
-internal class InvalidEventPayloadException(
+internal class InvalidEventPayloadException @JvmOverloads constructor(
     cause: Throwable? = null,
     description: String
 ) : ConsentLibExceptionK(
     cause = cause,
-    description = description){
+    description = description) {
     override val code: String = "invalid_event_payload"
 }
 
-internal class ResourceNotFoundException(
+internal class ResourceNotFoundException @JvmOverloads constructor(
     cause: Throwable? = null,
     description: String
 ) : ConsentLibExceptionK(
     cause = cause,
-    description = description){
+    description = description) {
     override val code: String = "resource_not_found"
 }
 
-internal class InvalidResponseException(
+internal class InvalidResponseException @JvmOverloads constructor(
     cause: Throwable? = null,
     description: String
 ) : ConsentLibExceptionK(
     cause = cause,
-    description = description){
+    description = description) {
     override val code: String = "invalid_response"
 }
 
-internal class InvalidLocalDataException(
+internal class InvalidLocalDataException @JvmOverloads constructor(
     cause: Throwable? = null,
     description: String
 ) : ConsentLibExceptionK(
     cause = cause,
-    description = description){
+    description = description) {
     override val code: String = "invalid_local_data"
 }
 
-internal class ConnectionTimeoutException(
+internal class ConnectionTimeoutException @JvmOverloads constructor(
     cause: Throwable? = null,
     description: String
 ) : ConsentLibExceptionK(
     cause = cause,
-    description = description){
+    description = description) {
     override val code: String = "connection_timeout"
 }
 
