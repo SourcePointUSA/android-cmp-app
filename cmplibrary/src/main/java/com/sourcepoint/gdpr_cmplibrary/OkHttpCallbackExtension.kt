415fec379bf47600c14d4ea0f674703abdd4f95c
Sourcepoint <carmelo@sourcepoint.com>
2020-12-18 12:58:46 +0100

    added test extension

--- cmplibrary/src/main/java/com/sourcepoint/gdpr_cmplibrary/OkHttpCallbackExtension.kt
+++ cmplibrary/src/main/java/com/sourcepoint/gdpr_cmplibrary/OkHttpCallbackExtension.kt
@@ -1,35 +1,32 @@
 package com.sourcepoint.gdpr_cmplibrary
 
-import android.util.Log
 import okhttp3.Call
 import okhttp3.Callback
 import okhttp3.Response
 import java.io.IOException
 
-fun Call.enqueue(block : OkHttpCallbackImpl.() -> Unit){
+fun Call.enqueue(block: OkHttpCallbackImpl.() -> Unit) {
     enqueue(OkHttpCallbackImpl().apply(block))
 }
 
-class OkHttpCallbackImpl : Callback{
-    
-    private var onFailure_ : ((call: Call, exception: IOException) -> Unit)? = null
-    private var onResponse_ : ((call: Call, r: Response) -> Unit)? = null
-    
-    fun onFailure(init : ((call: Call, exception: IOException) -> Unit)) {
+class OkHttpCallbackImpl : Callback {
+
+    private var onFailure_: ((call: Call, exception: IOException) -> Unit)? = null
+    private var onResponse_: ((call: Call, r: Response) -> Unit)? = null
+
+    fun onFailure(init: ((call: Call, exception: IOException) -> Unit)) {
         onFailure_ = init
     }
 
-    fun onResponse(init : ((call: Call, r: Response) -> Unit)) {
+    fun onResponse(init: ((call: Call, r: Response) -> Unit)) {
         onResponse_ = init
     }
-    
+
     override fun onFailure(call: Call, e: IOException) {
-        Log.d("ELogger", e.stackTraceToString())
         onFailure_?.invoke(call, e)
     }
 
     override fun onResponse(call: Call, r: Response) {
-        Log.d("ELogger", r.toString())
         onResponse_?.invoke(call, r)
     }
 }
--- cmplibrary/src/test/java/com/sourcepoint/gdpr_cmplibrary/TestUtils.kt
+++ cmplibrary/src/test/java/com/sourcepoint/gdpr_cmplibrary/TestUtils.kt
@@ -5,6 +5,8 @@ import okio.Buffer
 import org.junit.Assert
 
 infix fun <T> T.assertEquals(t: T) = apply { Assert.assertEquals(t, this) }
+fun <T : Any?> T.assertNotNull() = apply { Assert.assertNotNull(this) }
+fun <T : Any?> T.assertNull() = apply { Assert.assertNull(this) }
 
 fun Request.readText(): String {
     val buffer = Buffer()
