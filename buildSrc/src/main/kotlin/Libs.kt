5575627013af2b57006ccb7c3f1ffe5239ae9c05
Sourcepoint <carmelo@sourcepoint.com>
2021-01-06 15:37:04 +0100

    added kotlin kts support

---
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -0,0 +1,9 @@
+import kotlin.String
+
+object Libs {
+    const val kotlinxCoroutinesCore: String = "org.jetbrains.kotlinx:kotlinx-coroutines-core:" + Versions.coroutinesVersion
+    const val kotlinReflect: String = "org.jetbrains.kotlin:kotlin-reflect:" + Versions.kotlinVersion
+    const val mockk: String = "io.mockk:mockk:" + Versions.mockkVersion
+}
+
+
