d2fcdd444050614f5a6b100f0556d911504fada5
ciriti <ciriti@gmail.com>
2021-01-28 17:56:40 +0100

    added property account

--- samples/app/src/main/java/com/sourcepoint/example_app/MainActivityV6.java
+++ samples/app/src/main/java/com/sourcepoint/example_app/MainActivityV6.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
+import com.sourcepoint.cmplibrary.Account;
 import com.sourcepoint.cmplibrary.Builder;
 import com.sourcepoint.cmplibrary.legislation.gdpr.GDPRConsentLib;
 import com.sourcepoint.cmplibrary.legislation.gdpr.SpGDPRClient;
@@ -16,15 +17,20 @@ import kotlin.Lazy;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Objects;
+
 import static org.koin.java.KoinJavaComponent.inject;
 
 public class MainActivityV6 extends AppCompatActivity {
+
     private static final String TAG = "**MainActivity";
 
-    final static int accountId = 22;
-    final static int propertyId = 7639;
-    final static String propertyName = "tcfv2.mobile.webview";
-    final static String pmId = "122058";
+    private Account account = new Account(
+            22,
+            7639,
+            "tcfv2.mobile.webview",
+            "122058"
+    );
 
     GDPRConsentLib gdprConsent = null;
 
@@ -34,19 +40,20 @@ public class MainActivityV6 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findViewById(R.id.review_consents).setOnClickListener(_v -> gdprConsent.loadPrivacyManager());
-        findViewById(R.id.auth_id_activity).setOnClickListener(_v -> startActivity(new Intent(this, MainActivityAuthId.class)));
 
         gdprConsent = new Builder()
-                .setAccountId(accountId)
-                .setPropertyName(propertyName)
-                .setPropertyId(propertyId)
-                .setPmId(pmId)
+                .setAccountId(account.accountId)
+                .setPropertyName(account.propertyName)
+                .setPropertyId(account.propertyId)
+                .setPmId(account.pmId)
                 .setContext(this)
-                .setAuthId(dataProvider.getValue().getAuthId())
+                .setAuthId(Objects.requireNonNull(dataProvider.getValue().getAuthId()))
                 .build(GDPRConsentLib.class);
 
         gdprConsent.setSpGdprClient(new GdprClient());
+
+        findViewById(R.id.review_consents).setOnClickListener(_v -> gdprConsent.loadPrivacyManager());
+        findViewById(R.id.auth_id_activity).setOnClickListener(_v -> startActivity(new Intent(this, MainActivityAuthId.class)));
     }
 
     @Override
