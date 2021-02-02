014156121562f2401685b46840fc7ed6b65acf72
ciriti <ciriti@gmail.com>
2021-02-02 16:10:02 +0100

    added privacy manager link

---
+++ cmplibraryV6/src/main/java/com/sourcepoint/cmplibrary/legislation/gdpr/PrivacyManagerTabK.kt
@@ -0,0 +1,8 @@
+package com.sourcepoint.cmplibrary.legislation.gdpr
+
+enum class PrivacyManagerTabK(val key: String) {
+    DEFAULT(""),
+    PURPOSES("purposes"),
+    VENDORS("vendors"),
+    FEATURES("features")
+}
--- cmplibraryV6/src/test/java/com/sourcepoint/cmplibrary/data/network/util/HttpUrlManagerTest.kt
+++ cmplibraryV6/src/test/java/com/sourcepoint/cmplibrary/data/network/util/HttpUrlManagerTest.kt
@@ -1,6 +1,7 @@
 package com.sourcepoint.cmplibrary.data.network.util
 
 import com.sourcepoint.cmplibrary.assertEquals
+import com.sourcepoint.cmplibrary.legislation.gdpr.PrivacyManagerTabK
 import org.junit.Test
 
 class HttpUrlManagerTest {
@@ -17,5 +18,33 @@ class HttpUrlManagerTest {
         val sut = HttpUrlManagerSingleton.inAppUrlNativeMessage
         val url = sut.toString()
         url.assertEquals("https://cdn.privacy-mgmt.com/wrapper/tcfv2/v1/gdpr/native-message?inApp=true")
+    }
+
+    @Test
+    fun `GIVEN pmId and `() {
+        val config = PmUrlConfig(
+            consentUUID = "89b2d14b-70ee-4344-8cc2-1b7b281d0f2d",
+            pmTab = PrivacyManagerTabK.DEFAULT,
+            messageId = "122058",
+            siteId = "7639"
+        )
+        val sut = HttpUrlManagerSingleton.urlPm(config)
+        val url = sut.toString()
+        val expected = "https://cdn.privacy-mgmt.com/privacy-manager/index.html?consentLanguage=&consentUUID=89b2d14b-70ee-4344-8cc2-1b7b281d0f2d&site_id=7639&message_id=122058"
+        url.assertEquals(expected)
+    }
+
+    @Test
+    fun `GIVEN an OTT and `() {
+        val config = PmUrlConfig(
+            consentUUID = "3c7c3e22-0aac-4941-b97d-8e70b73b91c7",
+            pmTab = PrivacyManagerTabK.DEFAULT,
+            messageId = "122058",
+            siteId = "7639"
+        )
+        val sut = HttpUrlManagerSingleton.ottUrlPm(config)
+        val url = sut.toString()
+        val expected = "https://cdn.privacy-mgmt.com/privacy-manager-ott/index.html?consentLanguage=&consentUUID=3c7c3e22-0aac-4941-b97d-8e70b73b91c7&site_id=7639&message_id=122058"
+        url.assertEquals(expected)
     }
 }
