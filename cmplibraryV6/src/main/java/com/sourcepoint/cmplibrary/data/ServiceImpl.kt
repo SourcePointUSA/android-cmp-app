be9748f5b410ac1b4d66174d4a1ec505a0684af1
ciriti <ciriti@gmail.com>
2021-02-03 13:41:17 +0100

    service impl

---
+++ cmplibraryV6/src/main/java/com/sourcepoint/cmplibrary/data/ServiceImpl.kt
@@ -0,0 +1,37 @@
+package com.sourcepoint.cmplibrary.data
+
+import com.sourcepoint.cmplibrary.data.local.DataStorage
+import com.sourcepoint.cmplibrary.data.network.NetworkClient
+import com.sourcepoint.cmplibrary.data.network.model.MessageReq
+import com.sourcepoint.cmplibrary.data.network.model.MessageResp
+import com.sourcepoint.cmplibrary.data.network.model.NativeMessageResp
+
+internal fun Service.Companion.create(nc: NetworkClient, ds: DataStorage): Service = ServiceImpl(nc, ds)
+
+private class ServiceImpl(
+    private val nc: NetworkClient,
+    private val ds: DataStorage
+) : Service, NetworkClient by nc, DataStorage by ds {
+
+    override fun getMessage(messageReq: MessageReq, success: (MessageResp) -> Unit, error: (Throwable) -> Unit) {
+        nc.getMessage(
+            messageReq,
+            { messageResp ->
+                success(messageResp)
+                // TODO save the data into the local storage
+            },
+            ::error
+        )
+    }
+
+    override fun getNativeMessage(messageReq: MessageReq, success: (NativeMessageResp) -> Unit, error: (Throwable) -> Unit) {
+        nc.getNativeMessage(
+            messageReq,
+            { nativeMessageResp ->
+                success(nativeMessageResp)
+                // TODO save the data into the local storage
+            },
+            ::error
+        )
+    }
+}
