68b68200415ab22d15bf2023cf200b1c087b33d1
Carmelo Iriti <74296239+carmelo-iriti@users.noreply.github.com>
2021-02-05 13:46:53 +0100

    [SP-6061] sending sdk errors to the server only in production (#320)
    * added logger
    
    * added utility for debug
    
    * fix tests
    
    Co-authored-by: ciriti <ciriti@gmail.com>

--- cmplibrary/src/test/java/com/sourcepoint/gdpr_cmplibrary/MockLogger.kt
+++ cmplibrary/src/test/java/com/sourcepoint/gdpr_cmplibrary/MockLogger.kt
@@ -5,4 +5,7 @@ import com.sourcepoint.gdpr_cmplibrary.exception.Logger
 
 internal class MockLogger : Logger {
     override fun error(e: ConsentLibExceptionK) {}
+    override fun i(tag: String, msg: String) { }
+    override fun d(tag: String, msg: String) { }
+    override fun v(tag: String, msg: String) { }
 }
