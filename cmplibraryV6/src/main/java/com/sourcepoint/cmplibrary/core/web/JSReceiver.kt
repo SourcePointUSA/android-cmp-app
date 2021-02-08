9215221d43854cf8436b3963a74b299d0aef02ae
ciriti <ciriti@gmail.com>
2021-02-08 10:19:03 +0100

    change js receiver

--- cmplibraryV6/src/main/java/com/sourcepoint/cmplibrary/core/web/JSReceiver.kt
+++ cmplibraryV6/src/main/java/com/sourcepoint/cmplibrary/core/web/JSReceiver.kt
@@ -7,12 +7,8 @@ internal interface JSReceiver {
     fun log(tag: String?, msg: String?)
     @JavascriptInterface
     fun log(msg: String?)
-    // called when message or pm is about to be shown
     @JavascriptInterface
-    fun onConsentUIReady(isFromPM: Boolean)
-    // called when a choice is selected on the message
-    @JavascriptInterface
-    fun onAction(actionData: String)
+    fun onAction(actionData: String) // called when a choice is selected on the message
     @JavascriptInterface
     fun onError(errorMessage: String)
 
