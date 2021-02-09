9783b3e344f33f9884f31ac3777b6147dd199eb2
ciriti <ciriti@gmail.com>
2021-02-09 11:48:17 +0100

    added view into action callback

--- cmplibraryV6/src/main/java/com/sourcepoint/cmplibrary/core/web/JSClientLib.kt
+++ cmplibraryV6/src/main/java/com/sourcepoint/cmplibrary/core/web/JSClientLib.kt
@@ -1,8 +1,10 @@
 package com.sourcepoint.cmplibrary.core.web
 
+import android.view.View
 import android.webkit.WebView
 
 internal interface JSClientLib : JSReceiver {
     fun onConsentUIReady(isFromPM: Boolean, wv: WebView)
+    fun onAction(actionData: String, view: View)
     companion object
 }
