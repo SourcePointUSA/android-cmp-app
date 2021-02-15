f2fab59e013d770e616e934f61084b66554f7152
Sourcepoint <carmelo@sourcepoint.com>
2021-02-15 09:18:34 +0100

    added test storage gdpr

--- cmplibraryV6/src/androidTest/java/com/sourcepoint/cmplibrary/data/local/DataStorageGdprImplTest.kt
+++ cmplibraryV6/src/androidTest/java/com/sourcepoint/cmplibrary/data/local/DataStorageGdprImplTest.kt
@@ -6,6 +6,7 @@ import com.example.uitestutil.assertEquals
 import com.example.uitestutil.jsonFile2String
 import com.fasterxml.jackson.jr.ob.JSON
 import com.fasterxml.jackson.jr.ob.impl.DeferredMap
+import com.sourcepoint.cmplibrary.data.local.DataStorageGdpr.Companion.IABTCF_KEY_PREFIX
 import com.sourcepoint.cmplibrary.data.network.converter.toCCPA
 import com.sourcepoint.cmplibrary.data.network.converter.toGDPR
 import com.sourcepoint.cmplibrary.util.Either
@@ -61,5 +62,56 @@ class DataStorageGdprImplTest {
                 uc.uspstring.assertEquals(ccpa.userConsent.uspstring)
             }
         }
+    }
+
+    @Test
+    fun check_DataStorage_TcData_gets_stored() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        val storage = DataStorageGdpr.create(appContext).apply { clearAll() }
+        val map = DeferredMap(true)
+        (1..10).forEach { map["IABTCF_$it"] = "$it" }
+        storage.saveTcData(map)
+        val output = storage.getTcData()
+        map.forEach {
+            output[it.key].assertEquals(it.value)
+        }
+    }
+
+    @Test
+    fun clear_data_DataStorage() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        val storage = DataStorageGdpr.create(appContext).apply { clearAll() }
+
+        storage.saveAuthId("auth")
+        storage.saveConsentUuid("uuid")
+        storage.saveEuConsent("eu")
+        storage.saveMetaData("meta")
+        storage.saveAppliedLegislation("GDPR")
+        storage.saveTcData(getMap())
+
+        storage.getAuthId().assertEquals("auth")
+        storage.getConsentUuid().assertEquals("uuid")
+        storage.getEuConsent().assertEquals("eu")
+        storage.getMetaData().assertEquals("meta")
+        storage.getAppliedLegislation().assertEquals("GDPR")
+        storage.getTcData().assertEquals(getMap())
+
+        storage.clearInternalData()
+
+        /** clearInternalData delete only these prefs */
+        storage.getAuthId().assertEquals("")
+        storage.getConsentUuid().assertEquals("")
+        storage.getEuConsent().assertEquals("")
+        storage.getMetaData().assertEquals("")
+
+        /** clearInternalData DOES NOT delete these prefs */
+        storage.getAppliedLegislation().assertEquals("GDPR")
+        storage.getTcData().assertEquals(getMap())
+    }
+
+    private fun getMap(): DeferredMap {
+        return DeferredMap(false).apply { this["${IABTCF_KEY_PREFIX}key"] = "value" }
     }
 }
