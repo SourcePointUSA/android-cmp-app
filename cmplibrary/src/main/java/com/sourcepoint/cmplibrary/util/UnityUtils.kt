b6d2e5ef2f3f8d367c28afb51ab06acdb918f6b2
Fedko Dmytro <dmytro.f@gowombat.team>
2021-04-29 12:19:14 +0300

    Created UnityUtils with helper methods

---
+++ cmplibrary/src/main/java/com/sourcepoint/cmplibrary/util/UnityUtils.kt
@@ -0,0 +1,13 @@
+@file:JvmName("UnityUtils")
+
+package com.sourcepoint.cmplibrary.util
+
+import com.sourcepoint.cmplibrary.model.exposed.TargetingParam
+
+fun targetingParamArrayToList(array: Array<TargetingParam>): List<TargetingParam> {
+    return array.toList()
+}
+
+fun throwableToException(throwable: Throwable)  {
+    throw java.lang.Exception(throwable)
+}
