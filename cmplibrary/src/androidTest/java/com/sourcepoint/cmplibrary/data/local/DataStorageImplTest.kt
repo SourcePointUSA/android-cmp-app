6d77ce2dc30bf76ed5d665fea97ec8ca803fde63
Sourcepoint <carmelo@sourcepoint.com>
2021-05-05 14:03:46 +0200

    DataStorage completed

--- cmplibrary/src/androidTest/java/com/sourcepoint/cmplibrary/data/local/DataStorageImplTest.kt
+++ cmplibrary/src/androidTest/java/com/sourcepoint/cmplibrary/data/local/DataStorageImplTest.kt
@@ -3,6 +3,7 @@ package com.sourcepoint.cmplibrary.data.local
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import androidx.test.platform.app.InstrumentationRegistry
 import com.example.uitestutil.assertEquals
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -12,10 +13,15 @@ class DataStorageImplTest {
     private val appContext by lazy { InstrumentationRegistry.getInstrumentation().targetContext }
     private val gdprStorage by lazy { DataStorageGdpr.create(appContext) }
     private val ccpaStorage by lazy { DataStorageCcpa.create(appContext) }
+    private val sut by lazy { DataStorage.create(appContext, gdprStorage, ccpaStorage) }
+
+    @Before
+    fun setup() {
+        sut.clearAll()
+    }
 
     @Test
     fun check_used_key_for_save_get_LocalState() {
-        val sut = DataStorage.create(appContext, gdprStorage, ccpaStorage).apply { clearAll() }
         sut.run {
             saveLocalState("test_ls")
             getLocalState().assertEquals("test_ls")
@@ -24,7 +30,6 @@ class DataStorageImplTest {
 
     @Test
     fun check_used_key_for_save_get_PropertyPriorityData() {
-        val sut = DataStorage.create(appContext, gdprStorage, ccpaStorage).apply { clearAll() }
         sut.run {
             savePropertyPriorityData("test_ppd")
             getPropertyPriorityData().assertEquals("test_ppd")
@@ -33,7 +38,6 @@ class DataStorageImplTest {
 
     @Test
     fun check_used_key_for_save_get_PropertyId() {
-        val sut = DataStorage.create(appContext, gdprStorage, ccpaStorage).apply { clearAll() }
         sut.run {
             savePropertyId(100)
             getPropertyId().assertEquals(100)
