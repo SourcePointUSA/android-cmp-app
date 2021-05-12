f12998889f5c2a7daea13194caa060e9f5884577
Sourcepoint <carmelo@sourcepoint.com>
2021-05-12 15:43:40 +0200

    [SP-6968] MessageModelReqExt unit-tests

---
+++ cmplibrary/src/test/java/com/sourcepoint/cmplibrary/model/ext/MessageModelReqExtKtTest.kt
@@ -0,0 +1,44 @@
+package com.sourcepoint.cmplibrary.model.ext
+
+import com.sourcepoint.cmplibrary.assertEquals
+import com.sourcepoint.cmplibrary.model.toTreeMap
+import com.sourcepoint.cmplibrary.uwMessDataTest
+import org.json.JSONObject
+import org.junit.Test
+
+class MessageModelReqExtKtTest {
+
+    private val testJsonObject = JSONObject(
+        """
+            {
+              "accountId": 1,
+              "propertyHref": "http://com.test",
+              "campaigns": {
+                "gdpr": {
+                  "targetingParams": "{\"location\":\"EU\"}",
+                  "campaignEnv": "stage"
+                },
+                "ccpa": {
+                  "targetingParams": "{\"location\":\"US\"}",
+                  "campaignEnv": "stage"
+                }
+              },
+              "consentLanguage": "EN",
+              "requestUUID": "test",
+              "includeData": {
+                "messageMetaData": {},
+                "localState": {
+                  "type": "string"
+                },
+                "TCData": {}
+              }
+            }
+        """.trimIndent()
+    )
+
+    @Test
+    fun `GIVEN a UnifiedMessageRequest object RETURN stringify request Json obj`() {
+        val sut = JSONObject(uwMessDataTest.toBodyRequest())
+        sut.toTreeMap().assertEquals(testJsonObject.toTreeMap())
+    }
+}
