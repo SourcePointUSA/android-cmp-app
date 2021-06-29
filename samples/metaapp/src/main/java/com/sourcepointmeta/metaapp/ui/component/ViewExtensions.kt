b9220afc6401f8fd845dfe415b7d2774ee8d9209
Sourcepoint <carmelo@sourcepoint.com>
2021-06-29 12:09:03 +0200

    added colors

--- samples/metaapp/src/main/java/com/sourcepointmeta/metaapp/ui/component/ViewExtensions.kt
+++ samples/metaapp/src/main/java/com/sourcepointmeta/metaapp/ui/component/ViewExtensions.kt
@@ -1,21 +1,12 @@
 package com.sourcepointmeta.metaapp.ui.component
 
-import android.annotation.SuppressLint
 import android.app.AlertDialog
-import android.view.View
 import com.google.android.material.chip.Chip
 import com.google.android.material.chip.ChipGroup
 import com.sourcepointmeta.metaapp.R
-import com.sourcepointmeta.metaapp.core.getOrNull
 import com.sourcepointmeta.metaapp.data.localdatasource.MetaLog
-import com.sourcepointmeta.metaapp.util.check
-import kotlinx.android.synthetic.main.add_property_fragment.view.*
-import kotlinx.android.synthetic.main.item_action_demo.view.*
-import kotlinx.android.synthetic.main.item_log.view.*
-import kotlinx.android.synthetic.main.property_item.view.*
-import kotlinx.android.synthetic.main.property_item.view.chip_ccpa
-import kotlinx.android.synthetic.main.property_item.view.chip_gdpr
-import org.json.JSONObject
+import kotlinx.android.synthetic.main.item_action_demo.view.* // ktlint-disable
+import kotlinx.android.synthetic.main.property_item.view.* // ktlint-disable
 
 fun PropertyItemView.bind(
     item: PropertyDTO
@@ -35,72 +26,6 @@ fun PropertyItemView.bind(
 
 fun DemoActionItemView.bind(item: DemoActionItem) {
     action_item.text = item.message
-}
-
-@SuppressLint("ResourceType")
-fun LogItemView.bind(item: LogItem, position: Int) {
-    when (item.type) {
-        "REQUEST" -> bindReq(item, position)
-        "RESPONSE" -> bindResp(item, position)
-        "WEB_ACTION" -> bindWebAction(item, position)
-        "ERROR" -> bindClientError(item, position)
-        "CLIENT_EVENT" -> bindClientEvent(item, position)
-        "COMPUTATION" -> bindComputation(item, position)
-        else -> throw RuntimeException("No type found!!!")
-    }
-}
-
-fun LogItemView.bindReq(item: LogItem, position: Int) {
-    val url = item.message
-    log_title.text = "${item.type} - ${item.tag}"
-    log_body.text = url
-    item.jsonBody
-        ?.let { log_body_1.text = if (it.length > 200) it.subSequence(0, 200) else it }
-        ?: run { log_body_1.visibility = View.GONE }
-}
-
-fun LogItemView.bindResp(item: LogItem, position: Int) {
-    log_title.text = "${item.type} - ${item.tag}"
-    log_body.text = "Status: ${item.status}"
-    item.jsonBody
-        ?.let { log_body_1.text = if (it.length > 200) it.subSequence(0, 200) else it }
-        ?: run { log_body_1.visibility = View.GONE }
-}
-
-fun LogItemView.bindWebAction(item: LogItem, position: Int) {
-    log_title.text = "${item.type} - ${item.tag}"
-    log_body.text = item.message
-    item.jsonBody
-        ?.let { log_body_1.text = if (it.length > 200) it.subSequence(0, 200) else it }
-        ?: run { log_body_1.visibility = View.GONE }
-}
-
-fun LogItemView.bindComputation(item: LogItem, position: Int) {
-    log_title.text = "${item.type} - ${item.tag}"
-    log_body.visibility = View.GONE
-    log_body_1.text = item.message
-}
-
-fun LogItemView.bindClientEvent(item: LogItem, position: Int) {
-    log_title.text = "${item.type} - ${item.tag}"
-    val errorObject = item.jsonBody?.let { check { JSONObject(it) }.getOrNull() } ?: JSONObject()
-    val title: String? = errorObject.getOrNull("title")
-    val stackTrace: String = errorObject.getOrNull("stackTrace") ?: ""
-    when (title) {
-        null -> log_body.text = item.message
-        else -> log_body.text = "$title - $stackTrace"
-    }
-    item.jsonBody
-        ?.let { log_body_1.text = if (it.length > 200) it.subSequence(0, 200) else it }
-        ?: run { log_body_1.visibility = View.GONE }
-}
-
-fun JSONObject.getOrNull(key: String): String? = if (has(key)) this.getString(key) else null
-
-fun LogItemView.bindClientError(item: LogItem, position: Int) {
-    log_title.text = "${item.type} - ${item.tag}"
-    log_body.visibility = View.GONE
-    log_body_1.text = if (item.message.length > 200) item.message.subSequence(0, 200) else item.message
 }
 
 fun ChipGroup.addChip(content: String) {
