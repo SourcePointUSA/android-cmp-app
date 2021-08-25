df61953c0bf9e5310e5df3794bd594c6e8ef8ed2
carmelo-iriti <carmelo@sourcepoint.com>
2021-08-25 11:59:34 +0200

    mockk for android

--- buildSrc/src/main/kotlin/Libs.kt
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -2,6 +2,7 @@ object Libs {
     const val kotlinxCoroutinesCore: String = "org.jetbrains.kotlinx:kotlinx-coroutines-core:" + Versions.coroutinesVersion
     const val kotlinReflect: String = "org.jetbrains.kotlin:kotlin-reflect:" + Versions.kotlinVersion
     const val mockk: String = "io.mockk:mockk:" + Versions.mockkVersion
+    const val mockkAndroid: String = "io.mockk:mockk-android:" + Versions.mockkVersion
     const val jacksonJr: String = "com.fasterxml.jackson.jr:jackson-jr-objects:" + Versions.jacksonJrVersion
     const val jacksonJrsTree: String = "com.fasterxml.jackson.jr:jackson-jr-stree:" + Versions.jacksonJrVersion
     const val mockwebserver = "com.squareup.okhttp3:mockwebserver:${Versions.mockWSVersion}"
