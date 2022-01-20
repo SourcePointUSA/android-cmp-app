ca5261d2b39e0871fa6b9a5052cda4250afa8d2e
carmelo-iriti <carmelo@sourcepoint.com>
2022-01-20 10:08:13 +0100

    fix tests

--- cmplibrary/src/main/java/com/sourcepoint/cmplibrary/data/network/util/HttpUrlManager.kt
+++ cmplibrary/src/main/java/com/sourcepoint/cmplibrary/data/network/util/HttpUrlManager.kt
@@ -116,7 +116,7 @@ internal object HttpUrlManagerSingleton : HttpUrlManager {
 
 enum class Env(val host: String, val pmHostGdpr: String, val pmHostCcpa: String, val queryParam: String) {
     STAGE("cdn.sp-stage.net", "notice.sp-stage.net", "ccpa-notice.sp-stage.net", BuildConfig.ENV_QUERY_PARAM),
-    PRE_PROD("preprod-cdn.privacy-mgmt.com", "cdn.privacy-mgmt.com", "ccpa-inapp-pm.sp-prod.net", "stage"),
+    PRE_PROD("preprod-cdn.privacy-mgmt.com", "cdn.privacy-mgmt.com", "ccpa-inapp-pm.sp-prod.net", "prod"),
     PROD("cdn.privacy-mgmt.com", "cdn.privacy-mgmt.com", "cdn.privacy-mgmt.com", "prod")
 }
 
