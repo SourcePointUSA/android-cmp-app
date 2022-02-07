1ba44e6717239ac7a666de25ad9e88eaf5195ddb
carmelo-iriti <carmelo@sourcepoint.com>
2022-02-07 11:44:17 +0100

    added tests

--- cmplibrary/src/test/java/com/sourcepoint/cmplibrary/model/ext/MessageModelReqExtKtTest.kt
+++ cmplibrary/src/test/java/com/sourcepoint/cmplibrary/model/ext/MessageModelReqExtKtTest.kt
@@ -4,10 +4,23 @@ import com.sourcepoint.cmplibrary.assertEquals
 import com.sourcepoint.cmplibrary.data.network.model.toBodyRequest
 import com.sourcepoint.cmplibrary.model.toTreeMap
 import com.sourcepoint.cmplibrary.uwMessDataTest
+import com.sourcepoint.cmplibrary.uwMessDataTestPubData
 import org.json.JSONObject
 import org.junit.Test
 
 class MessageModelReqExtKtTest {
+
+    @Test
+    fun `GIVEN a UnifiedMessageRequest object RETURN stringify request Json obj`() {
+        val sut = JSONObject(uwMessDataTest.toBodyRequest())
+        sut.toTreeMap().assertEquals(testJsonObject.toTreeMap())
+    }
+
+    @Test
+    fun `GIVEN a UnifiedMessageRequest object with pubData RETURN stringify request Json obj`() {
+        val sut = JSONObject(uwMessDataTestPubData.toBodyRequest())
+        sut.toTreeMap().assertEquals(testJsonObjectPubData.toTreeMap())
+    }
 
     private val testJsonObject = JSONObject(
         """
@@ -47,9 +60,46 @@ class MessageModelReqExtKtTest {
         """.trimIndent()
     )
 
-    @Test
-    fun `GIVEN a UnifiedMessageRequest object RETURN stringify request Json obj`() {
-        val sut = JSONObject(uwMessDataTest.toBodyRequest())
-        sut.toTreeMap().assertEquals(testJsonObject.toTreeMap())
-    }
+    private val testJsonObjectPubData = JSONObject(
+        """
+            {
+              "accountId": 1,
+              "propertyHref": "http://com.test",
+              "pubData": {
+                "key_1": true,
+                "key_2": "test_pb",
+                "key_3": 1
+              },
+              "campaigns": {
+                "gdpr": {
+                  "targetingParams": {
+                    "location": "EU"
+                  }
+                },
+                "ccpa": {
+                  "targetingParams": {
+                    "location": "US"
+                  }
+                }
+              },
+              "consentLanguage": "EN",
+              "requestUUID": "test",
+              "includeData": {
+                "customVendorsResponse": {
+                  "type": "RecordString"
+                },
+                "messageMetaData": {
+                  "type": "RecordString"
+                },
+                "localState": {
+                  "type": "RecordString"
+                },
+                "TCData": {
+                  "type": "RecordString"
+                }
+              },
+              "campaignEnv": "stage"
+            }
+        """.trimIndent()
+    )
 }
