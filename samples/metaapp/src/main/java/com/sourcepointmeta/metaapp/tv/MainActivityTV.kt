67b315a3d830f97ff20ba9a38cbc27fb8a1f241b
Dmytro Fedko <fedko.dmytro@gmail.com>
2022-07-19 12:52:58 +0200

    [DIA-796] renames to comply code style and minor fixes

--- samples/metaapp/src/main/java/com/sourcepointmeta/metaapp/tv/MainActivityTV.kt
+++ samples/metaapp/src/main/java/com/sourcepointmeta/metaapp/tv/MainActivityTV.kt
@@ -9,7 +9,7 @@ class MainActivityTV : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity) // ?
+        setContentView(R.layout.main_activity)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, PropertyListFragmentTV())
