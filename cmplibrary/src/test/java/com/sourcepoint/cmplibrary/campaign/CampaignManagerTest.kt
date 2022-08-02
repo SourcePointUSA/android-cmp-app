d6e427a77d10fe2b62a0062f1fa154979ae6d255
carmelo-iriti <carmelo@sourcepoint.com>
2022-08-02 16:16:17 +0200

    [DIA-1054] IABUSPrivacy_String inconsistency in DataStorage

--- cmplibrary/src/test/java/com/sourcepoint/cmplibrary/campaign/CampaignManagerTest.kt
+++ cmplibrary/src/test/java/com/sourcepoint/cmplibrary/campaign/CampaignManagerTest.kt
@@ -161,6 +161,7 @@ class CampaignManagerTest {
             spConfig.copy(messageLanguage = MessageLanguage.BULGARIAN)
         )
         val config = sut.getPmConfig(CampaignType.GDPR, "22", PMTab.PURPOSES).getOrNull().assertNotNull()!!
+
         config.run {
             pmTab.assertEquals(PMTab.PURPOSES)
             consentLanguage.assertEquals("BG")
@@ -168,6 +169,7 @@ class CampaignManagerTest {
             siteId.assertEquals("0")
             messageId.assertEquals("22")
         }
+
     }
 
     @Test
