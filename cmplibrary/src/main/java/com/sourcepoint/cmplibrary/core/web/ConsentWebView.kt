23f0ae563e624cf4b2f6e5872987111fd6dc8bfd
carmelo-iriti <carmelo@sourcepoint.com>
2022-08-12 15:19:31 +0200

    detailed screen

--- cmplibrary/src/main/java/com/sourcepoint/cmplibrary/core/web/ConsentWebView.kt
+++ cmplibrary/src/main/java/com/sourcepoint/cmplibrary/core/web/ConsentWebView.kt
@@ -195,7 +195,6 @@ internal class ConsentWebView(
 
         @JavascriptInterface
         override fun onAction(actionData: String) {
-            println("JavascriptInterface: $actionData")
             checkWorkerThread("ConsentWebView on action")
             val action = actionData.toConsentAction()
             if (action.actionType == ActionType.PM_DISMISS && currentCampaignModel != null) {
