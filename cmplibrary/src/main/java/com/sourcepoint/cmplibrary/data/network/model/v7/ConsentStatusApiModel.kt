3eeb45656e5ac72d551d7fc6b62d0c438ccdaf45
carmelo-iriti <carmelo@sourcepoint.com>
2022-08-29 16:34:11 +0200

    consent-status api

---
+++ cmplibrary/src/main/java/com/sourcepoint/cmplibrary/data/network/model/v7/ConsentStatusApiModel.kt
@@ -0,0 +1,8 @@
+package com.sourcepoint.cmplibrary.data.network.model.v7
+
+import org.json.JSONObject
+
+internal class ConsentStatusResp(
+    val thisContent: JSONObject,
+    val localState: JSONObject
+)
