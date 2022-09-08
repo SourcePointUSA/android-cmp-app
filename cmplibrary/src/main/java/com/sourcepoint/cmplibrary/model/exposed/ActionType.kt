8c5407282710345c6db912b9b4e4c90706561972
carmelo-iriti <carmelo@sourcepoint.com>
2022-09-08 12:17:53 +0200

    [1006] Support Native OTT message

--- cmplibrary/src/main/java/com/sourcepoint/cmplibrary/model/exposed/ActionType.kt
+++ cmplibrary/src/main/java/com/sourcepoint/cmplibrary/model/exposed/ActionType.kt
@@ -24,5 +24,6 @@ enum class NativeMessageActionType(val code: Int) {
 enum class MessageSubCategory(val code: Int) {
     TCFv2(5),
     NATIVE_IN_APP(6),
-    OTT(7)
+    OTT(7),
+    NATIVE_OTT(14)
 }
