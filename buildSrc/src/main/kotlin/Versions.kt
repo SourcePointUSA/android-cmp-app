d060be4d29e5198df9d0b0c98f2882960d3f3eaf
bohdan-go-wombat <bohdan.s@gowombat.team>
2023-06-23 17:02:44 +0300

    mockk version change

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -2,7 +2,7 @@ object Versions {
     const val jacksonJrVersion: String = "2.12.1"
     const val coroutinesVersion: String = "1.4.1"
     const val kotlinVersion: String = "1.5.10"
-    const val mockkVersion: String = "1.10.3-jdk8"
+    const val mockkVersion: String = "1.13.5"
     const val mockWSVersion: String = "4.4.0"
     const val robolectricVersion: String = "4.4"
     const val androidxTestVersion: String = "1.3.0"
