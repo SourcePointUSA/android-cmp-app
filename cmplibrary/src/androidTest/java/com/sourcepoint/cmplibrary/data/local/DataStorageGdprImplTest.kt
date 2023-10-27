cc3e43e684d135bcb1e897d77233c91c266c73dc
Carmelo Iriti <74296239+carmelo-iriti@users.noreply.github.com>
2023-10-27 10:15:47 +0200

    release/7.4.3 (#730)

--- cmplibrary/src/androidTest/java/com/sourcepoint/cmplibrary/data/local/DataStorageGdprImplTest.kt
+++ cmplibrary/src/androidTest/java/com/sourcepoint/cmplibrary/data/local/DataStorageGdprImplTest.kt
@@ -3,7 +3,6 @@ package com.sourcepoint.cmplibrary.data.local
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import androidx.test.platform.app.InstrumentationRegistry
 import com.example.uitestutil.assertEquals
-import com.example.uitestutil.assertTrue
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -17,16 +16,13 @@ class DataStorageGdprImplTest {
         val storage = DataStorageGdpr.create(appContext).apply { clearAll() }
 
         storage.saveAuthId("auth")
-        storage.gdprApplies = true
         storage.saveGdpr("{\"type\":\"GDPR\"}")
 
-        storage.gdprApplies.assertTrue()
         storage.getGdpr().assertEquals("{\"type\":\"GDPR\"}")
 
         storage.clearInternalData()
 
         /** clearInternalData DOES NOT delete these prefs */
-        storage.gdprApplies.assertTrue()
         storage.getGdpr().assertEquals("{\"type\":\"GDPR\"}")
     }
 }
