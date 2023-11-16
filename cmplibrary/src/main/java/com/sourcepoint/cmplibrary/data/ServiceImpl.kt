f107416a5300661be9993a5efc3a25397a0c5d81
carmelo-iriti <carmelo@sourcepoint.com>
2023-11-16 11:37:54 +0100

    [DIA-2979] Handle PrivacyManager for usnat

--- cmplibrary/src/main/java/com/sourcepoint/cmplibrary/data/ServiceImpl.kt
+++ cmplibrary/src/main/java/com/sourcepoint/cmplibrary/data/ServiceImpl.kt
@@ -26,9 +26,9 @@ import com.sourcepoint.cmplibrary.model.* //ktlint-disable
 import com.sourcepoint.cmplibrary.model.exposed.GDPRPurposeGrants
 import com.sourcepoint.cmplibrary.model.exposed.SPConsents
 import com.sourcepoint.cmplibrary.util.check
+import com.sourcepoint.cmplibrary.util.extensions.* //ktlint-disable
 import com.sourcepoint.cmplibrary.util.extensions.extractIncludeGppDataParamIfEligible
 import com.sourcepoint.cmplibrary.util.extensions.isIncluded
-import com.sourcepoint.cmplibrary.util.extensions.toJsonObject
 import com.sourcepoint.cmplibrary.util.extensions.toMapOfAny
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.jsonObject
