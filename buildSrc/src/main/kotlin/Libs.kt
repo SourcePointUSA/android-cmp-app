6faa9ce95bdf380a99838669c8bd6825c8ec6eab
André Herculano <andresilveirah@gmail.com>
2024-08-27 13:06:52 +0200

    replace org.koin dependency with io.insert-koin

--- buildSrc/src/main/kotlin/Libs.kt
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -2,8 +2,8 @@ object Libs {
     const val kotlinxCoroutinesCore: String = "org.jetbrains.kotlinx:kotlinx-coroutines-core:" + Versions.coroutinesVersion
     const val mockk: String = "io.mockk:mockk:" + Versions.mockkVersion
     const val okHttpLatest = "com.squareup.okhttp3:okhttp:${Versions.okHttpLatestVersion}"
-    const val koinCore = "org.koin:koin-core:${Versions.koinVersion}"
-    const val koinCoreExt = "org.koin:koin-core-ext:${Versions.koinVersion}"
+    const val koinCore = "io.insert-koin:koin-core:${Versions.koinVersion}"
+    const val koinCoreExt = "io.insert-koin:koin-core-ext:${Versions.koinVersion}"
     const val koinAndroid = "io.insert-koin:koin-android:2.2.3"
     const val koinViewModel = "io.insert-koin:koin-androidx-viewmodel:2.2.3"
 
@@ -19,5 +19,3 @@ object Libs {
     const val leanback_pref : String = "androidx.leanback:leanback-preference:1.0.0"
 
 }
-
-
