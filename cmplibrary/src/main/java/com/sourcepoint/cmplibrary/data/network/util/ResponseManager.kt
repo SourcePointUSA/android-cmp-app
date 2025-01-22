dbc594b4e6a0c5ed21c860806699b34e6ee13f54
Dmytro <dmytro.f@gowombat.team>
2025-01-22 14:06:10 +0100

    Remove `choice` `parse` and `body` methods

--- cmplibrary/src/main/java/com/sourcepoint/cmplibrary/data/network/util/ResponseManager.kt
+++ cmplibrary/src/main/java/com/sourcepoint/cmplibrary/data/network/util/ResponseManager.kt
@@ -9,11 +9,6 @@ import com.sourcepoint.cmplibrary.data.network.model.optimized.choice.ChoiceType
 import okhttp3.Response
 
 internal interface ResponseManager {
-
-    fun parseGetChoiceResp(r: Response, choice: ChoiceTypeParam): ChoiceResp
-    fun parsePostGdprChoiceResp(r: Response): GdprCS
-    fun parsePostCcpaChoiceResp(r: Response): CcpaCS
-    fun parsePostUsNatChoiceResp(r: Response): USNatCS
     fun parseMessagesResp(r: Response): MessagesResp
 
     companion object
