f9606b25cfebd0ae4646262b865f62ca0dc5454a
André Herculano <andresilveirah@gmail.com>
2025-03-26 10:02:42 +0100

    remove unused code

--- cmplibrary/src/main/java/com/sourcepoint/cmplibrary/model/ConsentActionImpl.kt
+++ cmplibrary/src/main/java/com/sourcepoint/cmplibrary/model/ConsentActionImpl.kt
@@ -13,27 +13,9 @@ interface ConsentAction {
     val customActionId: String?
     val privacyManagerId: String?
     val choiceId: String?
-    val requestFromPm: Boolean
+    val requestFromPm: Boolean?
     val saveAndExitVariables: JSONObject
     val consentLanguage: String?
     val messageId: String
     val pmUrl: String?
 }
-
-internal data class ConsentActionImpl(
-    override val campaignType: CampaignType,
-    override val pubData: JSONObject = JSONObject(),
-    override val pubData2: JsonObject = JsonObject(mapOf()),
-    override val actionType: ActionType,
-    override val customActionId: String? = null,
-    override val privacyManagerId: String? = null,
-    override val choiceId: String? = null,
-    override val requestFromPm: Boolean,
-    override val saveAndExitVariables: JSONObject = JSONObject(),
-    override val consentLanguage: String? = MessageLanguage.ENGLISH.value,
-    val saveAndExitVariablesOptimized: JsonObject = JsonObject(mapOf()),
-    val pmTab: String? = null,
-    val thisContent: JSONObject = JSONObject(),
-    override val messageId: String,
-    override val pmUrl: String? = null
-) : ConsentAction
