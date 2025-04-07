d294f3b46d5bf8f4721c91dc4b5c4b1a1feb9437
André Herculano <andresilveirah@gmail.com>
2025-04-07 10:41:02 +0200

    pass GPPConfig to mobile-core

--- cmplibrary/src/main/java/com/sourcepoint/cmplibrary/model/exposed/Gpp.kt
+++ cmplibrary/src/main/java/com/sourcepoint/cmplibrary/model/exposed/Gpp.kt
@@ -1,18 +1,37 @@
 package com.sourcepoint.cmplibrary.model.exposed
 
+import com.sourcepoint.mobile_core.network.requests.IncludeData
+
 data class SpGppConfig(
     val coveredTransaction: SpGppOptionBinary?,
     val optOutOptionMode: SpGppOptionTernary?,
-    val serviceProviderMode: SpGppOptionTernary?,
-)
+    val serviceProviderMode: SpGppOptionTernary?
+) {
+    fun toCore() = IncludeData.GPPConfig(
+        MspaCoveredTransaction = coveredTransaction?.toCore(),
+        MspaOptOutOptionMode = optOutOptionMode?.toCore(),
+        MspaServiceProviderMode = serviceProviderMode?.toCore()
+    )
+}
 
 enum class SpGppOptionBinary(val type: String) {
     YES("yes"),
-    NO("no"),
+    NO("no");
+
+    fun toCore() = when (this) {
+        YES -> IncludeData.MspaBinaryFlag.yes
+        NO -> IncludeData.MspaBinaryFlag.no
+    }
 }
 
 enum class SpGppOptionTernary(val type: String) {
     YES("yes"),
     NO("no"),
-    NOT_APPLICABLE("na"),
+    NOT_APPLICABLE("na");
+
+    fun toCore() = when (this) {
+        YES -> IncludeData.MspaTernaryFlag.yes
+        NO -> IncludeData.MspaTernaryFlag.no
+        NOT_APPLICABLE -> IncludeData.MspaTernaryFlag.na
+    }
 }
